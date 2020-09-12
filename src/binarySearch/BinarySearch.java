d6b41953fc90768dc788e46d80cb7e46da4aab8c
Ram <ramkb466@gmail.com>
2020-09-12 11:45:38 -0400

    refined form of BS

--- src/binarySearch/BinarySearch.java
+++ src/binarySearch/BinarySearch.java
@@ -41,6 +41,24 @@ public class BinarySearch {
         }
        return -1; 
     }
+	
+	
+	int binarySearch(int[] nums, int target){
+		  if(nums == null || nums.length == 0)
+		    return -1;
+
+		  int left = 0, right = nums.length - 1;
+		  while(left <= right){
+		    // Prevent (left + right) overflow
+		    int mid = left + (right - left) / 2;
+		    if(nums[mid] == target){ return mid; }
+		    else if(nums[mid] < target) { left = mid + 1; }
+		    else { right = mid - 1; }
+		  }
+
+		  // End Condition: left > right
+		  return -1;
+		}
 
 	public static void main(String[] args) {
 		System.out.println(search(new int[] {2,5},5));
