c65f6a608386bd72533c08a5ff4ac6b7eca6ab6a
Ram <ramkb466@gmail.com>
2020-09-12 15:23:58 -0400

    terniary approach

--- src/binarySearch/GuessNumber.java
+++ src/binarySearch/GuessNumber.java
@@ -38,6 +38,41 @@ public class GuessNumber {
         // System.out.println(guess);
         return guess;
     }
+    
+    
+    /**
+     * In Binary Search, we choose the middle element as the pivot in splitting. 
+     * In Ternary Search, we choose two pivots (say m1m1 and m2m2) such that the given range is divided into three equal parts.
+     *  If the required number numnum is less than m1m1 then we apply ternary search on the left segment of m1m1. 
+     *  If numnum lies between m1m1 and m2m2, we apply ternary search between m1m1 and m2m2.
+     *   Otherwise we will search in the segment right to m2m2.
+     * @param n
+     * @return
+     */
+    
+    public int guessNumber2(int n) {
+        int low = 1;
+        int high = n;
+        while (low <= high) {
+            int mid1 = low + (high - low) / 3;
+            int mid2 = high - (high - low) / 3;
+            int res1 = guess(mid1);
+            int res2 = guess(mid2);
+            if (res1 == 0)
+                return mid1;
+            if (res2 == 0)
+                return mid2;
+            else if (res1 < 0)
+                high = mid1 - 1;
+            else if (res2 > 0)
+                low = mid2 + 1;
+            else {
+                low = mid1 + 1;
+                high = mid2 - 1;
+            }
+        }
+        return -1;
+    }
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
