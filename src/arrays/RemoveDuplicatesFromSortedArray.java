fce444b2a632da9aa86937518d2726aa1a6325c5
Ram <ramkb466@gmail.com>
2020-09-16 15:15:03 -0400

    3rd approach

--- src/arrays/RemoveDuplicatesFromSortedArray.java
+++ src/arrays/RemoveDuplicatesFromSortedArray.java
@@ -101,7 +101,43 @@ public class RemoveDuplicatesFromSortedArray {
 		}
 		// Return the new length.
 		return length;
-	}
+	} 
+	
+	
+	// approach 3
+	
+	public int[] copyWithRemovedDuplicates(int[] nums) {
+        
+		  // Check for edge cases.
+		  if (nums == null || nums.length == 0) {
+		      return nums;
+		  }
+
+		  // Count how many unique elements are in the Array.
+		  int uniqueNumbers = 0;
+		  for (int i = 0; i < nums.length; i++) {
+		      // An element should be counted as unique if it's the first
+		      // element in the Array, or is different to the one before it.
+		      if (i == 0 || nums[i] != nums[i - 1]) {
+		          uniqueNumbers++;
+		      }
+		  }
+
+		  // Create a result Array.
+		  int[] result = new int[uniqueNumbers];
+
+		  // Write the unique elements into the result Array.
+		  int positionInResult = 0;
+		  for (int i = 0; i < nums.length; i++) {
+		    // Same condition as in the previous loop. Except this time, we can write
+		    // each unique number into the result Array instead of just counting them.
+		      if (i == 0 || nums[i] != nums[i - 1]) {
+		          result[positionInResult] = nums[i];
+		          positionInResult++;
+		      }
+		  }
+		  return result;
+		}
 
 	public static void main(String[] args) {
 		System.out.println(removeDuplicates(new int[] {0,0,0,0,1,2,3,3,3,4}));//[1,2] [0,0,1,1,1,2,2,3,3,4] [0,0,0,0,1,2,3,3,3,4]
