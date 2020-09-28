190a96afdd1180e4818690878322cd91efc9445b
Ram <ramkb466@gmail.com>
2020-09-28 10:03:39 -0400

    iterative solution of pre-order traversal

---
+++ src/BinaryTree/PreOrderTraversal.java
@@ -0,0 +1,81 @@
+package BinaryTree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Given the root of a binary tree, return the preorder traversal of its nodes' values.
+
+ 
+
+Example 1:
+
+
+Input: root = [1,null,2,3]
+Output: [1,2,3]
+Example 2:
+
+Input: root = []
+Output: []
+Example 3:
+
+Input: root = [1]
+Output: [1]
+Example 4:
+
+
+Input: root = [1,2]
+Output: [1,2]
+Example 5:
+
+
+Input: root = [1,null,2]
+Output: [1,2]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up:
+
+Recursive solution is trivial, could you do it iteratively?
+ * @author ramkrishnabhattarai
+ *
+ */
+public class PreOrderTraversal {
+	
+	public class TreeNode {
+		      int val;
+		      TreeNode left;
+		      TreeNode right;
+		      TreeNode() {}
+		      TreeNode(int val) { this.val = val; }
+		      TreeNode(int val, TreeNode left, TreeNode right) {
+		          this.val = val;
+		          this.left = left;
+		          this.right = right;
+		      }
+	}
+	public List<Integer> preorderTraversal(TreeNode root) {
+		List<Integer> list  = new ArrayList<>();
+		if(root == null) return list;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while(!stack.empty()){
+            TreeNode node = stack.pop();
+            list.add(node.val);
+            if(node.right != null){
+                stack.push(node.right);
+            }
+            if(node.left != null){
+                stack.push(node.left);
+            }
+        }
+		return list;
+    }
+}
