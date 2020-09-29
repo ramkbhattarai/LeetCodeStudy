1c2db4acdf89a1b1730f20eaa281bfbebada1b25
Ram <ramkb466@gmail.com>
2020-09-29 09:58:52 -0400

    iterative solution using two stacks

--- src/BinaryTree/PostOrderTraversal.java
+++ src/BinaryTree/PostOrderTraversal.java
@@ -2,6 +2,7 @@ package BinaryTree;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 import BinaryTree.InorderTraversal.TreeNode;
 
@@ -30,6 +31,33 @@ public class PostOrderTraversal {
      }
      public List<Integer> postorderTraversal(TreeNode root) {
          return dfs(root, new ArrayList<Integer>());
+     }
+     
+     // iterative solution with 2 stacks
+     public List<Integer> postorderTraversal2(TreeNode root) {
+         List<Integer> list = new ArrayList<Integer>();
+         if(root == null) return list;
+         
+         Stack<TreeNode> stack1 = new Stack<TreeNode>();
+         Stack<TreeNode> stack2 = new Stack<TreeNode>();
+         stack1.push(root);
+         while(!stack1.empty()){
+            TreeNode node = stack1.pop();
+             stack2.push(node);
+             if(node.left != null){
+                 stack1.push(node.left);
+             }
+             if(node.right != null){
+                 stack1.push(node.right);
+             }
+         }
+
+          while(!stack2.empty()){
+            TreeNode node = stack2.pop();
+             list.add(node.val);
+         }
+
+         return list;
      }
 
 }
