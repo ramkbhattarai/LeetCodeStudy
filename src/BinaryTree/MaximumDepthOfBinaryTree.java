3647e08e32459c2bcc7aead06c44dc9949c66afd
Ram <ramkb466@gmail.com>
2020-09-30 09:34:09 -0400

    solution of maximum depth of tree using bottom up approach

---
+++ src/BinaryTree/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,51 @@
+package BinaryTree;
+
+import BinaryTree.LevelOrderTraversal.TreeNode;
+
+/**
+ * Given a binary tree, find its maximum depth.
+
+The maximum depth is the number of nodes along the longest path from the root node down to the farthest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its depth = 3.
+ * @author ramkrishnabhattarai
+ *
+ */
+public class MaximumDepthOfBinaryTree {
+	
+	public class TreeNode {
+	      int val;
+	      TreeNode left;
+	      TreeNode right;
+	      TreeNode() {}
+	      TreeNode(int val) { this.val = val; }
+	      TreeNode(int val, TreeNode left, TreeNode right) {
+	          this.val = val;
+	          this.left = left;
+	          this.right = right;
+	      }
+	  }
+	
+	// bottom-up recursive approach
+	public int maxDepth(TreeNode root) {
+        if (root == null) {
+		    return 0;                                   
+	    }
+	    int left_depth = maxDepth(root.left);
+	    int right_depth = maxDepth(root.right);
+	    return Math.max(left_depth, right_depth) + 1;
+    }
+	
+
+}
