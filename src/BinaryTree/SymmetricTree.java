85639523fedef28e81104b0400ca50ac10d71b86
Ram <ramkb466@gmail.com>
2020-09-30 09:42:23 -0400

    iterative approach

--- src/BinaryTree/SymmetricTree.java
+++ src/BinaryTree/SymmetricTree.java
@@ -1,5 +1,8 @@
 package BinaryTree;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 import BinaryTree.MaximumDepthOfBinaryTree.TreeNode;
 
 /**
@@ -52,5 +55,25 @@ public class SymmetricTree {
     public boolean isSymmetric(TreeNode root) {
          return isMirror(root, root);
     }
+    
+    
+    // iterative approach
+     public boolean isSymmetric2(TreeNode root) {
+     Queue<TreeNode> q = new LinkedList<>();
+     q.add(root);
+     q.add(root);
+     while (!q.isEmpty()) {
+         TreeNode t1 = q.poll();
+         TreeNode t2 = q.poll();
+         if (t1 == null && t2 == null) continue;
+         if (t1 == null || t2 == null) return false;
+         if (t1.val != t2.val) return false;
+         q.add(t1.left);
+         q.add(t2.right);
+         q.add(t1.right);
+         q.add(t2.left);
+     }
+          return true;
+     }
 
 }
