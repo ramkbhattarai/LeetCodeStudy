23a5471895a89854f96f3016a32913388e14d03a
Ram <ramkb466@gmail.com>
2020-10-07 09:19:14 -0400

    3rd approach which is more managable

--- src/BinaryTree/BuildBinaryTreeUsingInOrderAndPostOrder.java
+++ src/BinaryTree/BuildBinaryTreeUsingInOrderAndPostOrder.java
@@ -67,4 +67,32 @@ public class BuildBinaryTreeUsingInOrderAndPostOrder {
         root.right = makeBinaryTree(inorder, postorder, end -i+1, end, index-1);
         return root;
     }
+    
+    
+    // 3rd appraoch more managable to understand
+    
+    public TreeNode buildTree3(int[] inorder, int[] postorder) {
+        int n = inorder.length;
+      if(n == 0) return null;
+      return makeBinaryTree3(inorder, 0 , n , postorder, 0, n);
+  }
+  
+  private TreeNode makeBinaryTree3(int[] inorder, int i1, int i2, int[] postorder, int p1, int p2){
+      if(i1 >= i2 || p1 >= p2) return null;
+      
+      TreeNode root = new TreeNode(postorder[p2 -1]);
+      
+      int it = 0;
+      for(int i =i1; i < i2; i++){
+          if(postorder[p2 -1] == inorder[i]){
+              it = i;
+              break;
+          }
+      }
+      int diff = it -i1;
+      
+      root.left = makeBinaryTree3(inorder, i1, i1 + diff,postorder, p1, p1+ diff);
+      root.right = makeBinaryTree3(inorder,  i1+diff+1, i2,postorder, p1+diff, p2-1);
+      return root;
+  }
 }
