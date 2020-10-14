6bfaa796b816ea705b459274f366c6d2143e6b23
Ram <ramkb466@gmail.com>
2020-10-14 16:44:43 -0400

    iterative approach for LCA

--- src/BinaryTree/LowestCommonAncestor.java
+++ src/BinaryTree/LowestCommonAncestor.java
@@ -1,5 +1,11 @@
 package BinaryTree;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.Stack;
+
 import BinaryTree.PostOrderTraversal.TreeNode;
 
 /**
@@ -49,4 +55,48 @@ public class LowestCommonAncestor {
         if(left == null && right == null) return null;
         return left != null ? left : right;
     }
+	
+	// iterative approach
+	 public TreeNode lowestCommonAncestor2(TreeNode root, TreeNode p, TreeNode q) {
+
+	        // Stack for tree traversal
+	        Stack<TreeNode> stack = new Stack<>();
+
+	        // HashMap for parent pointers
+	        Map<TreeNode, TreeNode> parent = new HashMap<>();
+
+	        parent.put(root, null);
+	        stack.push(root);
+
+	        // Iterate until we find both the nodes p and q
+	        while (!parent.containsKey(p) || !parent.containsKey(q)) {
+
+	            TreeNode node = stack.pop();
+
+	            // While traversing the tree, keep saving the parent pointers.
+	            if (node.left != null) {
+	                parent.put(node.left, node);
+	                stack.push(node.left);
+	            }
+	            if (node.right != null) {
+	                parent.put(node.right, node);
+	                stack.push(node.right);
+	            }
+	        }
+
+	        // Ancestors set() for node p.
+	        Set<TreeNode> ancestors = new HashSet<>();
+
+	        // Process all ancestors for node p using parent pointers.
+	        while (p != null) {
+	            ancestors.add(p);
+	            p = parent.get(p);
+	        }
+
+	        // The first ancestor of q which appears in
+	        // p's ancestor set() is their lowest common ancestor.
+	        while (!ancestors.contains(q))
+	            q = parent.get(q);
+	        return q;
+	    }
 }
